516bbc1fb5f9530ec792d7fd5c2b61032e94db80
sagar gajera <sagargajera@sagars-MacBook-Pro.local>
2025-01-04 16:28:42 +0530

    Added functionality

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,16 +10,34 @@ import {
     Grid,
     CircularProgress,
     Box,
+    Checkbox,
+    FormControlLabel,
 } from '@mui/material';
 import '../styles/customStyles.css';
-import { getDummyOrders } from '../data/dummy'
+import { getDummyOrders } from '../data/dummy';
+import AddressFields from './AddressFields';
+import EditableField from './EditableFields';
+import AddressDisplay from '../components/AddressDisplay';
 
 const Dashboard = () => {
     const [orders, setOrders] = useState([]);
     const [loading, setLoading] = useState(false);
     const [selectedOrder, setSelectedOrder] = useState(null);
     const [modalType, setModalType] = useState(''); // 'view', 'edit', 'shipment'
-    const [editedData, setEditedData] = useState({});
+    const [editableData, setEditableData] = useState({
+        productNameForShiprocket: '',
+        hsn: '',
+        unitPrice: '',
+        deadWeight: '',
+        length: '',
+        height: '',
+        width: '',
+        shiprocketQty: '',
+        billingAddress: {
+            name: '', address1: '', address2: '', city: '', state: '', postalCode: '', country: ''
+        }
+    });
+
     const [shipmentDetails, setShipmentDetails] = useState({
         productNameForShiprocket: '',
         hsn: '',
@@ -39,11 +57,9 @@ const Dashboard = () => {
         setLoading(true);
         try {
             const response = await axios.get('http://localhost:8080/walmart/unshipped/orders');
-            console.log("unshipped", response.data)
             setOrders(response.data || []);
         } catch (error) {
             console.error('Error fetching orders:', error);
-            console.log("bscd " + getDummyOrders())
             setOrders(getDummyOrders());
         } finally {
             setLoading(false);
@@ -51,49 +67,100 @@ const Dashboard = () => {
     };
 
     const handleModalOpen = (order, type) => {
-        setSelectedOrder(order);
+        setSelectedOrder({ ...order, editableData: { ...editableData, ...order.editableData } });
         setModalType(type);
-        setEditedData(order);
-        if (type === 'shipment') {
-            setShipmentDetails({
-                productNameForShiprocket: '',
-                hsn: '',
-                unitPrice: '',
-                deadWeight: '',
-                length: '',
-                height: '',
-                width: '',
-                shiprocketQty: '',
-            });
-        }
     };
 
     const handleModalClose = () => {
-
         setOrders(prevOrders =>
             prevOrders.map(order =>
-                order.purchaseOrderId === selectedOrder.purchaseOrderId
+                order.customerOrderId === selectedOrder.customerOrderId
                     ? { ...order, ...selectedOrder }
                     : order
             )
         );
-
         setSelectedOrder(null);
         setModalType('');
     };
 
     const handleShipmentChange = (order, field, value) => {
-        setSelectedOrder({ ...order, [field]: value })
-        console.log(order[field])
+        setSelectedOrder({ ...order, editableData: { ...order.editableData, [field]: value } })
             ; setShipmentDetails({ ...shipmentDetails, [field]: value });
     };
 
     const handleEditChange = (field, value) => {
-        setEditedData({ ...editedData, [field]: value });
+
+        setSelectedOrder(prev => ({
+            ...prev,
+            editableData: {
+                ...prev.editableData,
+                billingAddress: {
+                    ...prev.editableData.billingAddress,
+                    [field]: value,
+                }
+            }
+        }));
+
     };
 
+    const handleEditBillingAddress = (field, value) => {
+
+        setSelectedOrder(prev => ({
+            ...prev,
+            editableData: {
+                ...prev.editableData,
+                billingAddress: {
+                    ...prev.editableData.billingAddress,
+                    [field]: value,
+                }
+            }
+        }));
+
+    };
+
+    const handleAddressCopy = (checked) => {
+        if (checked) {
+            setSelectedOrder(prev => ({
+                ...prev,
+                editableData: {
+                    ...prev.editableData,
+                    billingAddress: { ...prev.shippingInfo.postalAddress }
+                }
+            }));
+        } else {
+            setSelectedOrder(prev => ({
+                ...prev,
+                editableData: {
+                    ...prev.editableData,
+                    billingAddress: {
+                        name: '',
+                        address1: '',
+                        address2: '',
+                        city: '',
+                        state: '',
+                        postalCode: '',
+                        country: '',
+                    },
+                }
+            }));
+        }
+    };
+
+    const handleSave = () => {
+
+        setOrders(prevOrders =>
+            prevOrders.map(order =>
+                order.customerOrderId === selectedOrder.customerOrderId
+                    ? { ...order, ...selectedOrder }
+                    : order
+            )
+        );
+        handleModalClose();
+    };
+
+
     const handleSubmitShipment = async () => {
-        if (Object.values(shipmentDetails).some((value) => !value)) {
+        if (Object.values(shipmentDetails).some(value => !value)) {
             alert('Please fill all fields before submitting.');
             return;
         }
@@ -110,14 +177,6 @@ const Dashboard = () => {
         }
     };
 
-    const handleSave = () => {
-        const updatedOrders = orders.map((order) =>
-            order.purchaseOrderId === selectedOrder.purchaseOrderId ? { ...editedData } : order
-        );
-        setOrders(updatedOrders);
-        handleModalClose();
-    };
-
     if (loading) return <CircularProgress />;
 
     return (
@@ -126,11 +185,25 @@ const Dashboard = () => {
                 Unshipped Orders
             </Typography>
             <Grid container spacing={3}>
-                {orders.map((order) => (
-                    <Grid style={{margin: '10px 0px 10px 0px'}} item xs={12} md={6} lg={4} key={order.purchaseOrderId}>
-                        <Card style={{ height: '100%', display: 'flex', flexDirection: 'column', padding: 16}}>
+                {orders.map(order => (
+                    <Grid
+                        style={{ margin: '10px 0px' }}
+                        item
+                        xs={12}
+                        md={6}
+                        lg={4}
+                        key={order.customerOrderId}
+                    >
+                        <Card
+                            style={{
+                                height: '100%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                padding: 16,
+                            }}
+                        >
                             <CardContent>
-                                <Typography variant="h6">Order ID: {order.purchaseOrderId}</Typography>
+                                <Typography variant="h6">Order ID: {order.customerOrderId}</Typography>
                                 <Typography>Customer Name: {order.shippingInfo.postalAddress.name}</Typography>
                                 <Typography>Address: {order.shippingInfo.postalAddress.address1}</Typography>
                                 <Typography>Email: {order.customerEmailId}</Typography>
@@ -138,36 +211,27 @@ const Dashboard = () => {
                                 <Typography>Product Name: {order.orderInfo.productName}</Typography>
                                 <Typography>Quantity: {order.orderInfo.qtyAmount}</Typography>
                                 <Typography>Price: ${order.orderInfo.chargeAmount}</Typography>
-
-                                {order.productNameForShiprocket != null &&
-                                    <Typography>ProductName for shipment: {order.productNameForShiprocket}</Typography>}
-
-                                {order.hsn != null &&
-                                    <Typography>HSN: {order.hsn}</Typography>}
-
-                                {order.unitPrice != null &&
-                                    <Typography>UnitPrice : {order.unitPrice}</Typography>}
-
-                                {order.deadWeight != null &&
-                                    <Typography>DeadWeight : {order.deadWeight}</Typography>}
-
-                                {order.length != null &&
-                                    <Typography>Length: {order.length}</Typography>}
-
-                                {order.height != null &&
-                                    <Typography>Height: {order.height}</Typography>}
-                                {order.width != null &&
-                                    <Typography>Width  :{order.width}</Typography>}
+                                <EditableField data={order.editableData}></EditableField>
 
                             </CardContent>
                             <Box mt="auto" display="flex" gap={2}>
-                                <Button variant="contained" onClick={() => handleModalOpen(order, 'view')}>
+                                <Button
+                                    variant="contained"
+                                    onClick={() => handleModalOpen(order, 'view')}
+                                >
                                     View
                                 </Button>
-                                <Button variant="outlined" onClick={() => handleModalOpen(order, 'edit')}>
+                                <Button
+                                    variant="outlined"
+                                    onClick={() => handleModalOpen(order, 'edit')}
+                                >
                                     Edit
                                 </Button>
-                                <Button variant="contained" color="secondary" onClick={() => handleModalOpen(order, 'shipment')}>
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    onClick={() => handleModalOpen(order, 'shipment')}
+                                >
                                     Start Shipment
                                 </Button>
                             </Box>
@@ -176,25 +240,66 @@ const Dashboard = () => {
                 ))}
             </Grid>
 
-            <Modal open={!!modalType} onClose={handleModalClose}>
-                <Box sx={{ p: 4, bgcolor: 'background.paper', m: 'auto', maxWidth: 500, mt: 10 }}>
+            <Modal
+                open={!!modalType}
+                onClose={handleModalClose}
+                style={{ overflowY: 'auto' }}
+            >
+                <Box
+                    sx={{
+                        p: 4,
+                        bgcolor: 'background.paper',
+                        m: 'auto',
+                        maxWidth: 500,
+                        mt: 10,
+                        maxHeight: '80vh',
+                        overflowY: 'auto',
+                    }}
+                >
                     {modalType === 'view' && selectedOrder && (
-                        <>
-                            <Typography variant="h6">Order Details</Typography>
-                            <Typography>Order ID: {selectedOrder.purchaseOrderId}</Typography>
-                            <Typography>Customer Name: {selectedOrder.shippingInfo.postalAddress.name}</Typography>
-                        </>
+                        <Box>
+                            <Typography variant="h5" gutterBottom>
+                                Order Details
+                            </Typography>
+                            <Typography><b>Order ID:</b> {selectedOrder.customerOrderId}</Typography>
+                            <Typography><b>Customer Email:</b> {selectedOrder.customerEmailId}</Typography>
+
+                            <AddressDisplay address={selectedOrder.shippingInfo.postalAddress} addressType="Shipping Address"></AddressDisplay>
+
+                            {selectedOrder.editableData.billingAddress && (
+                                <>
+                                    <AddressDisplay address={selectedOrder.editableData.billingAddress} addressType="Billing Address"></AddressDisplay>
+                                </>
+                            )}
+                        </Box>
                     )}
 
                     {modalType === 'edit' && (
                         <>
                             <Typography variant="h6">Edit Order</Typography>
-                            <TextField
-                                label="Customer Name"
-                                value={editedData?.shippingInfo?.postalAddress?.name || ''}
-                                fullWidth
-                                onChange={(e) => handleEditChange('customerName', e.target.value)}
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        onChange={(e) => handleAddressCopy(e.target.checked)}
+                                    />
+                                }
+                                label="Same as Shipping Address"
                             />
+                            <AddressFields
+                                prefix="billingAddress"
+                                address={selectedOrder.editableData.billingAddress || {}}
+                                handleChange={handleEditBillingAddress}
+                            />
+                            {Object.keys(selectedOrder.editableData).map(field => (
+                                (field !== "billingAddress" && <TextField
+                                    key={field}
+                                    label={field}
+                                    value={selectedOrder.editableData[field] || ''}
+                                    fullWidth
+                                    margin="normal"
+                                    onChange={(e) => handleShipmentChange(selectedOrder, field, e.target.value)}
+                                />
+                                )))}
                             <Button variant="contained" onClick={handleSave}>
                                 Save
                             </Button>
@@ -204,20 +309,46 @@ const Dashboard = () => {
                     {modalType === 'shipment' && (
                         <>
                             <Typography variant="h6">Start Shipment</Typography>
-                            {Object.keys(shipmentDetails).map((field) => (
-                                <TextField
+
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        onChange={(e) => handleAddressCopy(e.target.checked)}
+                                    />
+                                }
+                                label="Same as Shipping Address"
+                            />
+                            <AddressFields
+                                prefix="billingAddress"
+                                address={selectedOrder.editableData.billingAddress || {}}
+                                handleChange={handleEditBillingAddress}
+                            />
+
+                            {Object.keys(selectedOrder.editableData).map(field => (
+                                (field !== "billingAddress" && <TextField
                                     key={field}
                                     label={field}
-                                    value={selectedOrder[field]}
+                                    value={selectedOrder.editableData[field] || ''}
                                     fullWidth
                                     margin="normal"
                                     onChange={(e) => handleShipmentChange(selectedOrder, field, e.target.value)}
                                 />
-                            ))}
-                            <Button style={{ marginRight:'10px'}} variant="contained" onClick={handleSubmitShipment}>
+                                )))}
+
+
+
+                            <Button
+                                style={{ marginRight: '10px' }}
+                                variant="contained"
+                                onClick={handleSubmitShipment}
+                            >
                                 Submit
                             </Button>
-                            <Button  style={{ marginLeft:'10px'}} variant="outlined" onClick={handleModalClose}>
+                            <Button
+                                style={{ marginLeft: '10px' }}
+                                variant="outlined"
+                                onClick={handleModalClose}
+                            >
                                 Save
                             </Button>
                         </>
